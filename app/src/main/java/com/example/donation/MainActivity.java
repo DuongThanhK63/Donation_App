da3314dd0a19f2f0d5649777fb48f29b961fe664
DuongThanhK63 <duongthanhk63uet@gmail.com>
2021-12-21 16:26:55 +0700

    Bai 6

--- app/src/main/java/com/example/donation/MainActivity.java
+++ app/src/main/java/com/example/donation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.donation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -77,12 +78,13 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case R.id.menuReport : startActivity (new Intent(this, Report.class));
+                break;
         }
         return super.onOptionsItemSelected(item);
+
     }
     public void donateButtonPressed (View view)
     {
