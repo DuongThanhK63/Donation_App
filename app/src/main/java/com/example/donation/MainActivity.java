9b46a698f487198ba01241323e5df41841af10c9
DuongThanhK63 <duongthanhk63uet@gmail.com>
2021-12-21 15:18:05 +0700

    Bai 4

--- app/src/main/java/com/example/donation/MainActivity.java
+++ app/src/main/java/com/example/donation/MainActivity.java
@@ -23,12 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup paymentMethod;
     private ProgressBar progressBar;
     private NumberPicker amountPicker;
+    private int totalDonated = 0;
 
-
-    public void donateButtonPressed (View view)
-    {
-        Log.v("Donate", "Donate Pressed!");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +46,14 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.v("Donate", "Really got the donate button");
         }
-
         paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
         amountPicker.setMinValue(0);
         amountPicker.setMaxValue(1000);
-
+        progressBar.setMax(10000);
 
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -77,6 +71,18 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    public void donateButtonPressed (View view)
+    {
+        int amount = amountPicker.getValue();
+        int radioId = paymentMethod.getCheckedRadioButtonId();
+        String method = radioId == R.id.PayPal ? "PayPal" : "Direct";
+        totalDonated = totalDonated + amount;
+        progressBar.setProgress(totalDonated);
+        Log.v("Donate", "Donate Pressed! with amount " + amount + ", method: " +
+                method);
+        Log.v("Donate", "Current total " + totalDonated);
+
     }
 
 
